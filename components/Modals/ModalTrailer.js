55445f92f521087ef12039485105863424f2a2b8
geovannygil <geovannygr2018@gmail.com>
2022-08-23 19:26:25 -0600

    test(Root): Modify link of the trailer video test

--- components/Modals/ModalTrailer.js
+++ components/Modals/ModalTrailer.js
@@ -4,7 +4,7 @@ export default function ModalTrailer ({ title, root }) {
   return (
     <Modal title={title} root={root}>
       <iframe
-        src='https://www.youtube.com/embed/4V7rfombLkc'
+        src='https://www.youtube.com/embed/HUcihIUUEy8'
         title='YouTube video player'
         frameBorder='0'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
