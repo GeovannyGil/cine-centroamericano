1a9ef7740758823d2c18b6ed938aa990efa3f9be
geovannygil <geovannygr2018@gmail.com>
2022-09-08 00:37:28 -0600

    feat(Root): Add marquee efect at movie

--- components/commons/Movie.js
+++ components/commons/Movie.js
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import Marquee from 'react-fast-marquee'
+import { BiPlay } from 'react-icons/bi'
 // import PropTypes from 'prop-types'
 
 export const Movie = ({ id, movieUid, title, imageUrl, priority = false }) => {
@@ -21,8 +23,14 @@ export const Movie = ({ id, movieUid, title, imageUrl, priority = false }) => {
         />
       </div>
       <div className='cc__contain-title-movie'>
-        <p>{title}</p>
-        <Link href='/movie/[id]' as={`/movie/${movieUid}`}><a className='cc__btn cc__btn-primary-color'>▶</a></Link>
+        <Marquee
+          className='marquee-text'
+          gradientWidth={50}
+          speed={75}
+        >
+          {title}
+        </Marquee>
+        <Link href='/movie/[id]' as={`/movie/${movieUid}`}><a className='cc__btn-primary-color'><BiPlay /></a></Link>
       </div>
     </div>
     // </Link>
