d99e0c64f0cd3e8178768acbad2a0ad9b195b04e
geovannygil <geovannygr2018@gmail.com>
2022-09-10 18:16:30 -0600

    feat(Root): Add Script GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import Script from 'next/script'
 import '../styles/globals.scss'
 
 function MyApp ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-6KVXTN4YHC' />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6KVXTN4YHC');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
